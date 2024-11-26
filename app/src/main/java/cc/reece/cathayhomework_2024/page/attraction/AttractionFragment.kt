46ce4174f00d1190de05ca185e36afcf033d4a3d
reece <loveuph114@gmail.com>
2024-11-27 01:00:32 +0800

    show infomation

--- app/src/main/java/cc/reece/cathayhomework_2024/page/attraction/AttractionFragment.kt
+++ app/src/main/java/cc/reece/cathayhomework_2024/page/attraction/AttractionFragment.kt
@@ -1,14 +1,15 @@
 package cc.reece.cathayhomework_2024.page.attraction
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.addCallback
 import androidx.core.os.bundleOf
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
-import cc.reece.cathayhomework_2024.R
 import cc.reece.cathayhomework_2024.databinding.FragmentAttractionBinding
 import cc.reece.cathayhomework_2024.model.Attraction
 import cc.reece.cathayhomework_2024.utils.getParcelableCompat
@@ -30,7 +31,7 @@ class AttractionFragment : Fragment() {
     private var _binding: FragmentAttractionBinding? = null
     private val binding get() = _binding!!
 
-    private val attraction get() = requireArguments().getParcelableCompat<Attraction>(KEY_ATTRACTION)
+    private val attraction get() = requireArguments().getParcelableCompat<Attraction>(KEY_ATTRACTION)!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,6 +44,7 @@ class AttractionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupWindow()
         setupViews()
         setupBack()
     }
@@ -52,15 +54,48 @@ class AttractionFragment : Fragment() {
         _binding = null
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWindow() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.appBarLayout.updatePadding(
+                top = insets.top,
+            )
+            binding.scrollerContent.updatePadding(
+                bottom = insets.bottom
+            )
+            WindowInsetsCompat.CONSUMED
+        }
+    }
+
     private fun setupViews() {
         binding.toolbar.apply {
+            title = attraction.name
             setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
-            title = getString(R.string.news_title)
             setNavigationOnClickListener { handleBackPressed() }
         }
-
-
+        binding.titleTextView.text = attraction.name
+        binding.addressTextView.text = attraction.address
+        binding.openTimeTextView.text = attraction.openTime
+        binding.telTextView.text = attraction.tel
+        binding.categoryTimeTextView.text = attraction.category.joinToString(" · ") { it.name }
+        binding.descTextView.text = attraction.introduction.trim()
+        binding.urlButton.apply {
+            isEnabled = attraction.url.isNotEmpty()
+            setOnClickListener {
+            }
+        }
+        binding.websiteButton.apply {
+            isEnabled = attraction.officialSite.isNotEmpty()
+            setOnClickListener { }
+        }
+        binding.facebookButton.apply {
+            isEnabled = attraction.facebook.isNotEmpty()
+            setOnClickListener { }
+        }
+        binding.ticketButton.apply {
+            isEnabled = attraction.ticket.isNotEmpty()
+            setOnClickListener { }
+        }
     }
 
     private fun setupBack() {
